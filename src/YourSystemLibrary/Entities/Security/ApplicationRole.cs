5412183860a3ba2d3d741e2b27d2110dc22005bf
Dan Gilleland <dgilleland@nait.ca>
2015-11-26 13:48:45 -0700

    Added a class for the ApplicationRole

---
+++ src/YourSystemLibrary/Entities/Security/ApplicationRole.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourSystemLibrary.Entities.Security
+{
+    public class ApplicationRole : IdentityRole
+    {
+    }
+}
